8f02b7f2d49aec6be82062d9ea1e2c08e8bc4943
Daanish ilahi <daanishsumkesula@gmail.com>
2025-01-15 16:49:10 +0530

    Update PatientVitalRepository.java

--- patientVitals/src/main/java/Repository/PatientVitalRepository.java
+++ patientVitals/src/main/java/Repository/PatientVitalRepository.java
@@ -2,6 +2,6 @@ package Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import Entity.PatientVital;
-
+   
 public interface PatientVitalRepository extends JpaRepository<PatientVital, Long> {
 }
